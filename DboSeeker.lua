a89d96b8c5740bae2f945f2023eec10467ea0605
Ivo Augusto Wanderley de Paiva <40743787+ivomastre@users.noreply.github.com>
2022-11-01 23:15:48 -0300

    added screenshot

--- DboSeeker.lua
+++ DboSeeker.lua
@@ -3,6 +3,7 @@ UI.Separator()
 
 local PlayerStorage = {}
 local Toogle = false
+local ScreenshotPath = "screenshot.png"
 
 toogleButton = UI.Button("Toogle Seeker: False", function()
     Toogle = not Toogle
@@ -24,25 +25,56 @@ toogleButton:setColor("red")
 
 UI.Separator()
 
-onPlayerPositionChange(function ()
+local function sendEvent(screenshotLink, location, playerName)
+    print("Sending event...")
+    print("Screenshot: "..screenshotLink)
+
+    HTTP.postJSON(storage.Url..'/discord/send', { 
+        userId=storage.Token, 
+        playerName=playerName,
+        cavebotName=storage._configs.cavebot_configs.selected,
+        location=location,
+        screenshotLink=screenshotLink
+    })
+end
+
+
+local function createScreenshot(location, playerName)
+    doScreenshot(ScreenshotPath);
+    
+    while not g_resources.fileExists(ScreenshotPath) do
+        print("Waiting for screenshot to be created...")
+    end
+    
+
+    HTTP.postJSON(
+        storage.Url.."/discord/file",
+        g_resources.readFileContents(ScreenshotPath),
+        function(response, err)
+            if err then screenshot = nil return end
+            
+            sendEvent(response.data.imageLink, location, playerName)
+        end
+    ); 
+
+
+    return screenshotLink
+end
+
+onCreaturePositionChange(function (creature)
     if (not Toogle) then return end
+    if (creature:isLocalPlayer()) then return end
+
     local location = 'X: '..posx()..' Y: '..posy()..' Z: '..posz()
 
-    for i, spec in ipairs(getSpectators()) do
-      if spec:isPlayer() and spec:getText() == "" and spec:getPosition().z == posz() and spec ~= player then
+    if creature:isPlayer() and creature:getText() == "" and creature:getPosition().z == posz() then
 
-          local playerName = spec:getName()
-          
-          if PlayerStorage[playerName] and PlayerStorage[playerName] > os.time() then return end
+        local playerName = creature:getName()
+        
+        if PlayerStorage[playerName] and PlayerStorage[playerName] > os.time() then return end
 
-          PlayerStorage[playerName] = os.time() + 600 -- 10 minutos
-          HTTP.postJSON(storage.Url..'/discord/send', { 
-            userId=storage.Token, 
-            playerName=playerName,
-            cavebotName=storage._configs.cavebot_configs.selected,
-            location=location
-          }) 
-      end
+        PlayerStorage[playerName] = os.time() + 600 -- 10 minutos
+        createScreenshot(location, playerName)
     end
 
 end)
